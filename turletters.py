c4c9a43dbc5b33a77239a6c91426eb46455f189a
carsonspotts <60612163+carsonspotts@users.noreply.github.com>
2020-02-06 09:26:00 -0500

    Update turletters.py

--- turletters.py
+++ turletters.py
@@ -97,18 +97,7 @@ def turtleLetter(letter,tur):
     elif letter == "T":
 	    pass
     elif letter == "U":
-	import turtle
-	turtle.forward(50)
-	turtle.left(90)
-	turtle.forward(100)
-	turtle.right(180)
-	turtle.forward(100)
-	turtle.right(90)
-	turtle.forward(50)
-	turtle.right(90)
-	turtle.forward(100)
-
-# 
+	    pass
     elif letter == "V":
 	    pass
     elif letter == "W":
